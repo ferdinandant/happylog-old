a1d4f715e29a9908e0681759ede63bd773abcac0
Ferdinand Antonius <ferdinand.antonius@traveloka.com>
2023-08-23 17:47:31 +0700

    Format float and complex

--- pkg/ctxparser/util_FormatAny.go
+++ pkg/ctxparser/util_FormatAny.go
@@ -32,8 +32,11 @@ func FormatAny(traversalCtx TraversalCtx) (result string, resultCtx *ParseResult
 	case reflect.Bool:
 		return FormatBool(traversalCtx), nil
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
-		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return FormatInteger(traversalCtx), nil
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64:
+		return FormatRealNumber(traversalCtx), nil
+	case reflect.Complex64, reflect.Complex128:
+		return FormatComplexNumber(traversalCtx), nil
 	case reflect.String:
 		return FormatString(traversalCtx), nil
 	// --- These are all complex types ---
